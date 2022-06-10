b91ff15da968f61d1a5bc818a8cd30bd20da5306
nirav <nirav.karad@saeculumsolutions.com>
2022-06-10 17:39:14 +0530

    sample node

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 3098);
+app.use(cors({ exposedHeaders: ["Link"] }));
+app.use(bodyParser.urlencoded({ extended: true, }));
+app.use(bodyParser.json({ limit: "50MB" }));
+
+app.post('/api/v1/pr-apikey/add', async (req, res) => {
+    res.status(200);
+    res.json({ status: 200 });
+});
